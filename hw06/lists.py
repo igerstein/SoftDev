46738caafb1e797e798edc8139b9fad70f9c3b59
igerstein <isaac.gerstein1@gmail.com>
2016-03-31 16:06:49 -0400

    Finished strength function and readded d3 transition hw

--- hw06/lists.py
+++ hw06/lists.py
@@ -16,6 +16,16 @@ def meetsThreshold(pw):
 # Returns the strength rating of a password from 1-10
 def strength(pw):
     L = [0 if x in lowerCase else 1 if x in upperCase else 2 if x in nums else 3 if x in nonAlph else 4 for x in pw]
-    
-
-print strength("abc123*?*")
+    length = len(L)
+    if length > 10:
+        length = 10
+    if not (0 in L or 1 in L or 2 in L or 3 in L):
+        return 0
+    elif not (1 in L or 2 in L or 3 in L):
+        return int(length * 0.6)
+    elif not (2 in L or 3 in L) or not (1 in L or 3 in L) or not (1 in L or 2 in L):
+        return int(length * 0.7)
+    elif not (0 in L and 1 in L and 2 in L and 3 in L):
+        return int(length * 0.8)
+    else:
+        return length
