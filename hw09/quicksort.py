38044ff14fc525d90ccfe8d195aa3c6609b4f58d
igerstein <isaac.gerstein1@gmail.com>
2016-04-11 12:23:05 -0400

    Improved quicksort

--- hw09/quicksort.py
+++ hw09/quicksort.py
@@ -3,8 +3,11 @@
 # HW09 -- Quicksort
 # 2016-04-11
 
+import random
+
 def quicksort(L):
     if len(L) <= 1:
         return L
     else:
-        return quicksort([x for x in L if x < L[0]]) + [L[0]] + quicksort([x for x in L if x > L[0]])
+        pivot = random.choice(L)
+        return quicksort([x for x in L if x < pivot]) + ([pivot] * L.count(pivot)) + quicksort([x for x in L if x > pivot])
