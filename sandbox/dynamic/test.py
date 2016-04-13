0429de1b6f00e57d9909123e9d6af57a668a3ca5
igerstein <isaac.gerstein1@gmail.com>
2016-04-13 12:32:53 -0400

    Added dynamic programming example

---
+++ sandbox/dynamic/test.py
@@ -0,0 +1,16 @@
+def memoize(f):
+    memo = {}
+    def inner(x):
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+    return inner
+
+@memoize
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
