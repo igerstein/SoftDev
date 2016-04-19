158df4bd330554b6c873381c1484d6cfd49ea974
igerstein <isaac.gerstein1@gmail.com>
2016-04-19 12:35:47 -0400

    Added javascript test code

---
+++ sandbox/javascript/index.js
@@ -0,0 +1,25 @@
+var base = "this is in global namespace";
+var x = "x in the global namespace";
+var f2 = function() {
+    console.log("this is f2");
+};
+
+var f1 = {
+    x: "x in f1 namespace",
+    f2: f2,
+    f: function() {
+	console.log("this is f in f1 namespace");
+	console.log("x is " + x);
+	console.log("f1.x is " + f1.x);
+	console.log("better way: " + this.x);
+    }
+};
+
+var makeIncrementer = function() {
+    var i = 0;
+    var inc = function() {
+	i = i + 1;
+	return i
+    }
+    return inc
+};
